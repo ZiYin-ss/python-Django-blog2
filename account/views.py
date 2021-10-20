bd97d496729d30770fa9f4261856019c726fdd2d
Zhao Qiang <980689346@qq.com>
2021-10-21 01:21:08 +0800

    'aa'

--- account/views.py
+++ account/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import *
+
 
 #  自己写的用户登录的判断  原生的 但我们用的是Django内置的
 def user_login(request):
@@ -24,4 +25,27 @@ def user_login(request):
         # 这个东西 你实例化 出来的就是html页面的 input框 用户名和密码的input框
         # 上面用这个数据 其实就是你在input输入了两个东西 就把那东西传递进去了 进行验证了
         login_form = LoginForm()
-        return render(request,"account/login.html",{"form":login_form})
+        return render(request, "account/login.html", {"form": login_form})
+
+
+def register(request):
+    if request.method == "POST":
+        userprofile_form = UserProfileForm(request.POST)
+        user_form = RegistrationForm(request.POST)
+        if user_form.is_valid()*userprofile_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            new_profile = userprofile_form.save(commit=False)
+            new_profile.user = new_user
+            # 因为上面只插入这个表是不是只插入这俩个字段  所以说 要把这次插入得对应user表得那个对象给他 会自己取出来id得 记住就好了
+            new_profile.save()
+            return HttpResponse("成功")
+        else:
+            return HttpResponse("对不起注册失败 不能注册")
+
+    else:
+        user_form = RegistrationForm()
+        userprofile_form = UserProfileForm()
+        return render(request, "account/register.html", {"form": user_form,"profile":userprofile_form})
+

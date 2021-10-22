43094197f887f3df03e65f4af5af59b3c2b7303c
Zhao Qiang <980689346@qq.com>
2021-10-22 22:51:47 +0800

    '显示后台文章'

--- article/views.py
+++ article/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+
 from .models import ArticleColumn
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
 from django.http import HttpResponse
 from .forms import ArticleColumnForm
-
-
 
 
 @login_required(login_url='/account/login/')
@@ -24,3 +24,31 @@ def article_column(request):
         else:  # 这个是没有 要是没有 就创建这条数据 如果你要拿变量接收的话你还可以用这条数据
             ArticleColumn.objects.create(user=request.user, column=column_name)
             return HttpResponse("1")
+
+@login_required(login_url='/account/login/')
+@require_POST
+@csrf_exempt
+def rename_article_column(request):
+    column_name = request.POST['column_name']
+    column_id = request.POST['column_id']
+    try:
+        # 就是原来的数据都可以修改 因为没有判断啊
+        line = ArticleColumn.objects.get(id=column_id)
+        line.column = column_name
+        line.save()
+        return HttpResponse("1")
+    except:
+        return HttpResponse("0")
+
+
+@login_required(login_url='/account/login')
+@require_POST
+@csrf_exempt
+def del_article_column(request):
+    column_id = request.POST["column_id"]
+    try:
+        line = ArticleColumn.objects.get(id=column_id)
+        line.delete()
+        return HttpResponse("1")
+    except:
+       return HttpResponse("2")
